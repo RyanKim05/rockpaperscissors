27144db15cda7d6a9842221b35e432be661e0b3e
RyanKim05 <ryankim1375@gmail.com>
2025-02-24 23:27:03 -0500

    Added UI and DOM event listeners

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -19,7 +19,7 @@ function getComputerChoice(){
 }
 
 
-
+/*
 //Get user input
 //Make sure that its a valid input
 function getHumanChoice(){
@@ -30,29 +30,87 @@ function getHumanChoice(){
     }
     return ask;
 }
-
+*/
 
 //Keeps track of score
 let playerScore=0;
 let computerScore=0;
+const result = document.createElement("span");
 
 //Simulates the round and updates winners score
 function playRound(humanChoice, computerChoice){
+    if(playerScore ===5 || computerScore ===5){
+        if(playerScore>computerScore){
+            winner = "Game over, You win!";
+        }
+        else{
+            winner = "Game over, You lose!";
+        }
+        result.innerText = winner;
+        playerScore = 0;
+        computerScore = 0;
+        return;
+    }
     h=humanChoice;
     c=computerChoice;
     if (h===c){
-        console.log("It was a tie!");
+        result.innerText = "It was a tie!";
     }
     else if ((h==="rock" && c==="paper") || (h==="paper" && c==="scissors") || (h==="scissors" && c==="rock")){
-        console.log("You Lose! " + c + " beats " + h);
+        result.innerText = "You Lose! " + c + " beats " + h;
         computerScore++;
     }
     else{
-        console.log("You win! " + h + " beats " + c);
+        result.innerText = "You win! " + h + " beats " + c;
         playerScore++;
     }
+    scores.innerText = `Player: ${playerScore} Computer: ${computerScore}`;
 }
 
+
+select = document.createElement("div");
+document.body.appendChild(select);
+
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+select.appendChild(rock);
+rock.innerText = "rock";
+
+select.appendChild(paper);
+paper.innerText = "paper";
+
+select.appendChild(scissors);
+scissors.innerText = "scissors";
+
+const scores = document.createElement("p");
+
+select.appendChild(scores);
+
+
+rock.addEventListener('click', ()=>{
+    playRound("rock", getComputerChoice());
+    select.appendChild(result);
+});
+
+paper.addEventListener('click', ()=>{
+    playRound("paper", getComputerChoice());
+    select.appendChild(result);
+});
+
+scissors.addEventListener('click', ()=>{
+    playRound("scissors", getComputerChoice());
+    select.appendChild(result);
+});
+
+select.style.display = "flex";
+select.style.justifyContent = "center";
+select.style.flexDirection = "column";
+select.style.alignItems = "center";
+select.style.gap = "10px";
+
+/*
 //Executes rounds for best to 5 game
 function playGame(){
     while(playerScore+computerScore<5){
@@ -62,3 +120,4 @@ function playGame(){
 }
 
 playGame();
+*/
